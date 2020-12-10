b892dd7232f92db260498d00109309e0402acdcc
Rafael Rocha <faelrocha07@gmail.com>
2020-12-10 19:39:06 -0300

    manually deploy stg

---
+++ precache-manifest.0b5947d692f39f90f3e1c2615b15773b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a605d73a6f3f8b65e2aa621046fdf73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b2dc15d664cf6792cd7",
+    "url": "/static/css/main.cb19e976.chunk.css"
+  },
+  {
+    "revision": "a554bdbee21dbec8277f",
+    "url": "/static/js/2.a79baf5e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a79baf5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2dc15d664cf6792cd7",
+    "url": "/static/js/main.1b13bf31.chunk.js"
+  },
+  {
+    "revision": "3730dcc4216ecb4b8ea7",
+    "url": "/static/js/runtime-main.36029b24.js"
+  }
+]);
